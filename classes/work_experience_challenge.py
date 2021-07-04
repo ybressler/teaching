930508c8a21268dc8882ed3bddcfc27c59098209
ybressler <yaakovbressler@gmail.com>
2021-07-04 12:32:03 -0400

    fleshed out class inheritance

--- classes/work_experience_challenge.py
+++ classes/work_experience_challenge.py
@@ -1,75 +1,86 @@
-"""
-This module contains information + is a guide for how and when to use
-classes in python.
-
-# Challenge:
-Say you are building a tool which allows recruiters more effectively
-screen candidates for a given job. The recruiter gives you a "wish list"
-of variables they'd like to screen for, but you (a wisened developer) anticipate
-that this list of requirements is likely to change, quite rapidly at that. With
-this in mind, you plan on building a solution which is maleable and easy to
-change.
-
-## Provided Data:
-You recieve data for a given person's past and current experiences. Your program
-must determine if this person fits the role, and to what extent.
-
-Example input data:
-```
-record = {
-    first_name: Ada,
-    last_name: Lovelace,
-    work_experience: [
-        {
-            company: University of Cambridge,
-            start_date: 1840-01-01,
-            end_date: 1848-01-01,
-            title: Software Developer
-        },
-        {
-            company: University of Cambridge,
-            start_date: 1848-01-01,
-            end_date: 1852-06-01,
-            title: Senior Software Developer
-        },
-    ]
-
-}
-```
-Some notes:
-    * Each person can have many work experiences
-    * A person cannot have concurrent jobs (each job they've had cannot
-        exist while they've had another job).
-        * If a start data precedes an end date, overwrite the start date to
-            the end date. Ex: job 1 ends 2015-01-15 and job 2 starts at
-            2015-01-01. Overwrite job 2 to start at 2015-01-15
-    * If an end date is not provided, assume that person still holds that job.
-
-
-## Requirements:
-Your recruiter would like the following attributes:
-* N years experience total
-    * N years experience in a senior role
-        * You must create and execute a definition for this.
-    * N years experience in a data role
-        * You must create and execute a definition for this.
-    * N years experience in a leadership role
-* Current job title
-* If the candidate is working > 1 year in their current role
-* Average length of time a candidate has worked at a company
-
------
-
-## Extra notes:
-* You must use classes and methods to execute everything.
-* Data will be provided in the accompanied json `work_experience_challenge.json`
-
-"""
-
-
 import os
 import json
+from datetime import datetime
 
-
+# Load your data
 with open('classes/work_experience_challenge.json', 'r') as f:
     data = json.load(f)
+
+
+class Person:
+    """Create a person to represent each person in the dataset"""
+    def __init__(self, first_name: str, last_name: str, **kwargs):
+        self.first_name = first_name
+        self.last_name = last_name
+
+        # If work experiences are provided, initialize them
+        if kwargs.get('work_experience'):
+            self.work_experience = [WorkExperience(**x) for x in kwargs.get('work_experience')]
+        else:
+            self.work_experience = []  # always init with no experiences
+
+
+class WorkExperience:
+    """Represents a work experience, regardless of whom"""
+
+    def __init__(self, company: str, title: str, start_date: datetime, end_date: datetime):
+        self.company = Company(company)
+        self.job_title = JobTitle(title)
+        self.start_date = self.validate_dt(start_date)
+        self.end_date = self.validate_dt(end_date)
+
+    @staticmethod
+    def validate_dt(dt):
+        """Convert to date if a string"""
+        # If no date is provided, set the dt to now
+        if dt is None:
+            return datetime.now()
+
+        if isinstance(dt, str):
+            return datetime.strptime(dt, '%Y-%m-%d')
+
+        else:
+            return dt
+
+
+class Company:
+    """Represent a company"""
+    def __init__(self, name: str):
+        self.name = name
+
+
+class JobTitleHelper:
+    """Use this class to add functionality to `JobTitle`"""
+    def __init__(self):
+        # One helper function
+        self.is_senior = 'senior' in self.name.lower()
+        # Add more helper functions here
+
+
+class JobTitle(JobTitleHelper):
+    """Represents a job title"""
+
+    def __init__(self, name: str):
+        # Set your own attributes
+        self.name = name
+
+        # Then, initialize `JobTitleHelper`
+        super().__init__()
+
+
+
+
+
+# -----------------------------------------------------------------------------
+
+my_people = []
+
+# Iterate through your records
+for record in data:
+    my_person = Person(**record)
+    my_people.append(my_person)
+
+
+[x.__dict__ for x in my_people]
+
+my_people[0].work_experience[0].job_title.__dict__
